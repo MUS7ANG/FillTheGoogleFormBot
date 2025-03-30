6439c33b51481a7e67000ec7306359ceabe9958b
MUS7ANG <salihjudo@icloud.com>
2025-03-30 22:20:42 +0600

    new token

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 import os
 from pytz import timezone
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7623595891:AAFVppQGyjImFUfK3rGsnUPq1k_ky5WHd_A")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7803855448:AAGmIMoPz2Dnd27XZZlihBD5hyJuP7DYhJw")
 
 NTP_SERVER = "asia.pool.ntp.org"
 
